f16d2d1fdf3a8cef8527a6b71c54167805ff2b72
-thugnasty- <eterictodd@gmail.com>
2018-12-01 15:06:22 -0700

    end of video #5

--- emdb/src/App.js
+++ emdb/src/App.js
@@ -3,14 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    toggle: true
+  };
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Using Props" />
+          <Welcome text="Welcome to Using Props" toggle={this.state.toggle} />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            {this.state.toggle && <p>This show and hide</p>}
+            <button onClick={this.toggle}>Show / Hide</button>
           </p>
           <a
             className="App-link"
@@ -28,7 +40,8 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
-    const { text } = this.props;
+    const { text, toggle } = this.props;
+    console.log(toggle);
     return <h1 className="App-title">{text}</h1>;
   }
 }
