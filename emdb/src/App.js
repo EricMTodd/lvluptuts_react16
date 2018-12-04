ae57fc1b704a3f9a55b60d9357c4eb16236e114a
-thugnasty- <eterictodd@gmail.com>
2018-12-03 20:56:36 -0700

    end of video #9

--- emdb/src/App.js
+++ emdb/src/App.js
@@ -2,57 +2,39 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "No Country For Old Men"
+  },
+  {
+    id: 2,
+    title: "Pulp Fiction"
+  },
+  {
+    id: 3,
+    title: "Spider-Man"
+  },
+  {
+    id: 4,
+    title: "Resevoir Dogs"
+  }
+];
+
 class App extends Component {
-  state = {
-    input: "Hello"
-  };
-
-  updateInput = event => {
-    this.setState({
-      input: event.target.value
-    });
-  };
-
-  submit = () => {
-    console.log(this.text.value);
-  };
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Using Props" />
-          <h3>
-            Edit <code>src/App.js</code> and save to reload.
-            <br />
-            <p>{this.state.input}</p>
-            <input
-              type="text"
-              onChange={this.updateInput}
-              value={this.state.input}
-            />
-            <input type="text" ref={input => (this.text = input)} />
-            <button onClick={this.submit}>Show Value</button>
-          </h3>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return <h1 className="App-title">{text}</h1>;
   }
 }
 
