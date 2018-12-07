0c25853b8ece3ab9ce59f99ba4e8a30c8d2fdf40
-thugnasty- <eterictodd@gmail.com>
2018-12-07 11:28:43 -0700

    end of video #13

--- emdb/src/App.js
+++ emdb/src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Movie from "./Movie";
+import Movie from './Movie';
 
 class App extends Component {
   state = {
-    movies: []
+    movies: [],
   };
 
   async componentDidMount() {
     try {
       const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=0604d3ffec907684cdf29df1d03a0c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+        'https://api.themoviedb.org/3/discover/movie?api_key=0604d3ffec907684cdf29df1d03a0c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
       );
       const movies = await res.json();
-      console.log("movies:", movies);
+      console.log('movies:', movies);
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch (error) {
-      console.log("error:", error);
+      console.log('error:', error);
     }
   }
 
