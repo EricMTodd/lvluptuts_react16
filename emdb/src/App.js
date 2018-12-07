11dbf103c928d8c3b50b9bd88d456b5e4921c056
-thugnasty- <eterictodd@gmail.com>
2018-12-07 10:59:18 -0700

    end of video #11

--- emdb/src/App.js
+++ emdb/src/App.js
@@ -4,35 +4,34 @@ import "./App.css";
 
 import Movie from "./Movie";
 
-const movies = [
-  {
-    id: 1,
-    title: "No Country For Old Men",
-    desc: "Good psychopath film"
-  },
-  {
-    id: 2,
-    title: "Pulp Fiction"
-  },
-  {
-    id: 3,
-    title: "Spider-Man"
-  },
-  {
-    id: 4,
-    title: "Resevoir Dogs"
-  }
-];
-
 class App extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=0604d3ffec907684cdf29df1d03a0c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      );
+      const movies = await res.json();
+      console.log("movies:", movies);
+      this.setState({
+        movies: movies.results
+      });
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie} desc={movie.desc} />
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
         ))}
       </div>
     );
