dceff051e151c0979008c43cc35c1669d281b166
-thugnasty- <eterictodd@gmail.com>
2018-12-09 09:35:55 -0700

    end of video #15

--- emdb/src/App.js
+++ emdb/src/App.js
@@ -1,42 +1,48 @@
 /* eslint react/no-did-mount-set-state: 0 */
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Movie from './Movie';
+import Movie from "./Movie";
 
 class App extends Component {
   state = {
-    movies: [],
+    movies: []
   };
 
   async componentDidMount() {
     try {
       const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=0604d3ffec907684cdf29df1d03a0c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+        "https://api.themoviedb.org/3/discover/movie?api_key=0604d3ffec907684cdf29df1d03a0c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
       );
       const movies = await res.json();
-      console.log('movies:', movies);
+      console.log("movies:", movies);
       this.setState({
-        movies: movies.results,
+        movies: movies.results
       });
     } catch (error) {
-      console.log('error:', error);
+      console.log("error:", error);
     }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Route path="/test" component={Test} />
+          {this.state.movies.map(movie => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+const Test = () => <h1>TEST</h1>;
